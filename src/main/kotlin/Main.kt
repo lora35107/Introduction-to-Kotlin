0ee7bb2678d7fef821837128364f7c7f4a09bab9
emobilis <lawrieabuna@gmail.com>
2023-03-14 12:49:20 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val length = 40
+    val width = 20
+    val area = length * width
+    print(area)
+}
---
+++ src/main/kotlin/bmicalculator.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val weight = 300
+    val height = 30
+    val bmi = weight / height
+    print(bmi)
+}
